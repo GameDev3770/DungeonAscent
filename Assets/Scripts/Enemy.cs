4e317425eb59df8fefc5ed395cf2ce996a1763f5
unknown <mehdi.youssef97@gmail.com>
2022-12-13 17:58:18 -0500

    d

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,7 +12,8 @@ public class Enemy : MonoBehaviour
     float maxticker = 100;
     float tick = 0;
 
-    private void Start() {
+    private void Start()
+    {
         this.Environment = GameObject.Find("Environment");
         this.Player = GameObject.Find("Player");
     }
@@ -25,30 +26,43 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void FixedUpdate() {
-        if (tick >= maxticker) {
+    private void FixedUpdate()
+    {
+        if (tick >= maxticker)
+        {
             Vector3 vec = transform.position;
             float[] temp = MakeMove();
             vec.x = temp[0];
             vec.z = temp[1];
             transform.position = vec;
             tick = 0;
+            //animation
         }
         tick++;
     }
 
-    void HitByRay() {
+    void HitByRay()
+    {
         health.TakeDamage(20);
     }
 
-    float[] MakeMove() {
+    float[] MakeMove()
+    {
         List<Node> output = Environment.GetComponent<PathFinderAPI>().GetPathing(this.gameObject, this.Player);
-        if (1 < output.Count && output.Count <= 5 && IsEmpty(new Vector3(output[0].x, 5, output[0].y))) {
+        if (1 < output.Count && output.Count <= 6 && IsEmpty(new Vector3(output[0].x, 5, output[0].y)))
+        {
             return new float[] { output[0].x, output[0].y };
-        } return new float[] { transform.position.x, transform.position.z };
+        } 
+        else if(output.Count == 1)
+        {
+            Debug.Log("Front");
+            //attack
+        }
+        return new float[] { transform.position.x, transform.position.z };
     }
 
-    public bool IsEmpty(Vector3 worldPosition) {
+    public bool IsEmpty(Vector3 worldPosition)
+    {
         RaycastHit hit;
         bool a = Physics.Raycast(worldPosition, Vector3.down, out hit, 10, 1 << LayerMask.NameToLayer("Entity"));
         return !a;
